9bc17527e6a03acc1c5843d71a19214ee3f6dd8a
IDerkBot <ivanderk2003@gmail.com>
2022-05-08 22:17:52 +0300

    Сделал запоминание данных при повторной авторизации

--- Views/Pages/AuthPage.xaml.cs
+++ Views/Pages/AuthPage.xaml.cs
@@ -17,19 +17,34 @@ namespace EventApp.Views.Pages
 		}
 		private void Auth_Click(object sender, RoutedEventArgs e)
 		{
-			if (EventEntities.GetContext().Users.Any(x => x.Login == Login.Text && x.Password == Password.Password))
-			{
-				PageManager.Navigate(new MenuPage());
+			if (EventEntities.GetContext().Users.Any(x => x.Login == TbLogin.Text))
+            {
+                var user = EventEntities.GetContext().Users.Single(x => x.Login == TbLogin.Text);
+
+                if (user.Password == PbPassword.Password)
+                {
+                    if (IsRemember.IsChecked == true) FileManager.SetConfig(new Config(user.Login, user.Password, true));
+                    PageManager.Navigate(new MenuPage());
+				}
+                else
+                    MessageBox.Show("Пароль не верный");
 			}
 			else
-			{
 				MessageBox.Show("Логин или пароль введены не верно");
-			}
 		}
 
 		private void Reg_Click(object sender, RoutedEventArgs e)
 		{
 			PageManager.Navigate(new RegPage());
 		}
-	}
+
+        private void AuthPage_OnLoaded(object sender, RoutedEventArgs e)
+        {
+			var config = FileManager.GetConfig();
+            if (!config.RememberMe) return;
+            TbLogin.Text = config.Login;
+            PbPassword.Password = config.Password;
+            IsRemember.IsChecked = true;
+		}
+    }
 }
