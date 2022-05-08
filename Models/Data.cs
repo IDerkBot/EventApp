b9503bc35e1b33bd99a5a33bbb8a570ec74e78d0
IDerkBot <ivanderk2003@gmail.com>
2022-05-08 22:17:37 +0300

    Добавил файл конфигурации

---
+++ Models/Data.cs
@@ -0,0 +1,9 @@
+﻿namespace EventApp.Models
+{
+	internal class Data
+	{
+		public static string CurrentDirectory { get; set; } = "EventApp";
+		public static string CurrentConfigFile { get; set; } = "config";
+		public static string CurrentConfigExtension { get; set; } = "json";
+	}
+}
